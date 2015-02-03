f8d059c371da60ba0e494b81be7fb5336d4820fe
Miguel Mota <miguelmota2@gmail.com>
2015-02-03 15:18:10 -0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ember Autocomplete Text Input
+
+#Demo
+
+**[http://lab.moogs.io/ember-autocomplete-text-input](http://lab.moogs.io/ember-autocomplete-text-input)**
+
+# Usage
+
+Take a look at the example in `example/`.
+
+```javascript
+{{autocomplete-text-input type="email" value=email data=domains shadowAfter="@" action="gotValue"}}
+```
+
+# License
+
+MIT
---
+++ example/ember-autocomplete-text-input.js
@@ -0,0 +1,67 @@
+App.AutocompleteTextInputComponent = Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['autocomplete-text-input'],
+  attributeBindings: [
+    'value',
+    'type',
+    'shadowValue',
+    'data',
+    'shadowAfter'
+  ],
+
+  value: null,
+  shadowValue: null,
+  data: [],
+  beginAfter: null,
+
+  didInsertElement: function() {
+    this.keyUp();
+  },
+
+  getDatum: function(val) {
+    if (!val) return '';
+    return this.get('data').filter(function(d) {
+      return d.indexOf(val) === 0;
+    })[0] || '';
+  },
+
+  keyUp: function(e) {
+    e = e || {};
+
+    var keys = {
+      ENTER: 13,
+      TAB: 9
+    };
+
+    var code = e.keyCode || e.which;
+
+    var value = this.get('value') || '';
+    var shadowAfter = this.get('shadowAfter');
+    var shadowValue = '';
+
+    if (shadowAfter) {
+      var re = new RegExp('(.*)(' + shadowAfter + ')(.*)', 'i');
+      var parts = value.match(re);
+      if (parts && parts[1] && parts[2] && parts[3]) {
+        shadowValue = parts[1] + parts[2] + this.getDatum(parts[3]);
+      }
+    } else {
+      shadowValue = this.getDatum(value);
+    }
+
+    this.set('shadowValue', shadowValue);
+
+    if (code === keys.ENTER || code === keys.TAB) {
+      this.set('value', shadowValue);
+      e.preventDefault();
+      this.sendAction('action', this.get('value'));
+    }
+  },
+
+  actions: {
+    keyPressed: function(value) {
+
+    }
+  }
+
+});
